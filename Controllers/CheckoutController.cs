6b16731966b10edb65ac3d2aeff937c45b1920b9
Daniel <bahmani00@gmail.com>
2021-02-19 22:28:08 -0500

    initial draft

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryApp.Data.Services;
+using LibraryApp.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly IBookService _courseService;
+
+        public CheckoutController(IBookService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Purchase(Guid id)
+        {
+
+            var book = _courseService.GetById(id);
+            if (book == null) return NotFound();
+
+            var data = new BookPurchaseVM
+            {
+                Id = book.Id,
+                Description = book.Description,
+                Author = book.Author,
+                Thumbnail = book.Thumbnail,
+                Title = book.Title,
+                Price = book.Price,
+                Nonce = ""
+            };
+
+            return View(data);
+        }
+    }
+}
