77581a1188ccaa08c7f68b3d0175dfa7105ccfbe
Daniel <bahmani00@gmail.com>
2021-02-20 11:09:04 -0500

    Add and setup braintreeGateway

---
+++ Data/Services/BraintreeService.cs
@@ -0,0 +1,19 @@
+﻿using Braintree;
+
+namespace LibraryApp.Data.Services
+{
+    public class BraintreeService : IBraintreeService
+    {
+        private readonly IBraintreeGateway braintreeGateway;
+
+        public BraintreeService(IBraintreeGateway braintreeGateway)
+        {
+            this.braintreeGateway = braintreeGateway;
+        }
+
+        public string GenerateToken()
+        {
+            return braintreeGateway.ClientToken.Generate();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,8 @@ namespace LibraryApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddBraintreeGateway(Configuration);
+
             services.AddTransient<IBookService, BookService>();
             services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase(new Guid().ToString()));
             services.AddControllersWithViews();
