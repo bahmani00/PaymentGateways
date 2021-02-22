0a06579507680c7eeba8e5447de404338bf8af14
Daniel <bahmani00@gmail.com>
2021-02-21 20:30:01 -0500

    impelemented "Admin Dashboard"

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using LibraryApp.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Controllers
+{
+    public class AdminController : Controller
+    {
+         private readonly IPaymentGatewayService _PaymentGatewayService;
+
+        public AdminController(IPaymentGatewayService paymentGatewayService)
+        {
+            _PaymentGatewayService = paymentGatewayService;
+        }
+        
+        public IActionResult Index()
+        {
+            var response = _PaymentGatewayService.GetTotalBalance();
+
+            return View(response);
+        }
+    }
+}
